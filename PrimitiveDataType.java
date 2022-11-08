ba6ea5811c39ac23564557e9867eabb39cd75f8e
xorz <xorztechnology@gmail.com>
2022-11-08 17:08:54 +0530

    Primitive Data Type

---
+++ PrimitiveDataType.java
@@ -0,0 +1,22 @@
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        boolean amILearning = true;
+        byte byteVar = 3;
+        char charVar = 'X';
+        short shortVar = 100;
+        int intVar = 101;
+        long longVar = 123456789L;
+        float floatVar = 1000.01f;
+        double doubleVar = 123456789.01d;
+
+
+        System.out.println(amILearning);
+        System.out.println(byteVar);
+        System.out.println(charVar);
+        System.out.println(shortVar);
+        System.out.println(intVar);
+        System.out.println(longVar);
+        System.out.println(floatVar);
+        System.out.println(doubleVar);
+    }
+}
