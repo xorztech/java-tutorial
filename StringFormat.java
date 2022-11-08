e85819eef0d8a0fb7bc1f168fbc4b714d05b839a
xorz <xorztechnology@gmail.com>
2022-11-08 16:48:48 +0530

    String Format

---
+++ StringFormat.java
@@ -0,0 +1,12 @@
+public class StringFormat {
+    public static void main(String[] args){
+        //String Format
+        String varString = "Xorz Technology!";
+        int varInt = 100;
+        //System.out.println(varString);
+        //System.out.format(varString);
+        //System.out.format("Welcome to %s", varString);
+        //System.out.format("Number %d", varInt);
+        System.out.format("Welcome to " + varString);
+    }
+}
