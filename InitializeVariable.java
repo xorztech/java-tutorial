a13f49616023fd3bec985ae0e80fae34790adb29
xorz <xorztechnology@gmail.com>
2022-11-08 12:08:25 +0530

    Java Tutorial: Initialize Variable

---
+++ InitializeVariable.java
@@ -0,0 +1,10 @@
+public class InitializeVariable {
+    public static void main(String[] args){
+        //initialize variable
+        final int var;
+        var = 100;
+
+        System.out.println(var);
+
+    }
+}
