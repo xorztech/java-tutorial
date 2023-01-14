c506dc66f9d80582935d18eabd3373117c3c6483
Xorz <xorztechnology@gmail.com>
2023-01-14 19:42:56 +0530

    Array and String

---
+++ ArrayAndString.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayAndString {
+    public static void main(String[] args) {
+        String stringVar = "Xorz Technology!";
+
+        System.out.println("String: " + stringVar);
+        System.out.println("Length: " + stringVar.length());
+        System.out.println("Lowercase: " + stringVar.toLowerCase());
+        System.out.println("Uppercase: " + stringVar.toUpperCase());
+
+        //Array
+
+        String[] javaTopics = {"Intro", "Setup", "Deployment"};
+        System.out.println("Array: " + javaTopics);
+        System.out.println("ArrayData: " + Arrays.toString(javaTopics));
+        System.out.println("Length: " + javaTopics.length);
+
+    }
+}
--- PrimitiveDataType.java
+++ PrimitiveDataType.java
@@ -1,12 +1,28 @@
 public class PrimitiveDataType {
     public static void main(String[] args) {
+
+        //1 Bit
         boolean amILearning = true;
+
+        //8 Bits or 1 Byte
         byte byteVar = 3;
+
+        //16 Bits or 2 Bytes
         char charVar = 'X';
+
+        //16 Bits or 2 Bytes
         short shortVar = 100;
+
+        //32 Bits or 4 Bytes
         int intVar = 101;
+
+        //64 Bits or 8 Bytes
         long longVar = 123456789L;
+
+        //32 Bits or 4 Bytes
         float floatVar = 1000.01f;
+
+        //64 Bits or 8 Bytes
         double doubleVar = 123456789.01d;
 
 
